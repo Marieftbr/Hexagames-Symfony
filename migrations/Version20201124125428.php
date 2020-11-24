35a5ed2d2cc016c4d6f3ee87cf7a3e65e0e0ad14
Marie Gigaud <mariegoulouzelle@gmail.com>
2020-11-24 15:52:34 +0100

    Toutes les migrations faites

---
+++ migrations/Version20201124125428.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201124125428 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE game CHANGE player_number player_number_max INT NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE game CHANGE player_number_max player_number INT NOT NULL');
+    }
+}
