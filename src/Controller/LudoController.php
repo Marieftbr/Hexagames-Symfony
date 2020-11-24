c3fb5fdb960913132f8b088a68411f669937c110
Marie Gigaud <mariegoulouzelle@gmail.com>
2020-11-24 15:53:03 +0100

    Méli mélo

--- src/Controller/LudoController.php
+++ src/Controller/LudoController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Form\LudoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +14,17 @@ class LudoController extends AbstractController
 {
     /**
      * @Route("/ludotheque")
+     * @param Request $request
+     * @return Response
      */
-    public function show(): Response
+    public function show(Request $request): Response
     {
         $form = $this->createForm(LudoType::class);
+        $form->handleRequest($request);
+        //data = $form->getData();
+
+
+
         return $this->render('ludotheque/la-ludotheque.html.twig', [
             'ludo_form' => $form->createView(),
         ]);
