f259b41d35cc2ecf87f24af3fea4bc6700dbe557
Marie Gigaud <mariegoulouzelle@gmail.com>
2020-12-07 20:54:10 +0100

    Setup basic authentication

--- src/Controller/GameProfileController.php
+++ src/Controller/GameProfileController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Comment;
 use App\Entity\Game;
+use App\Form\CommentType;
+use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class GameProfileController extends AbstractController
 {
@@ -15,8 +23,34 @@ class GameProfileController extends AbstractController
     public function index(Game $game): Response
     {
         return $this->render('game_profile/index.html.twig', [
-            'game' => $game
+            'game' => $game,
+            'categories' => $game->getCategories()
+
         ]);
+    }
+
+    /**
+     * @Route("/comment/add/{id}", name="game_add_comment", methods={"GET", "POST"})
+     */
+    public function addComment(Game $game, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setGame($game);
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+        }
+        return $this->render('game_profile/add.html.twig', [
+            'comment_form' => $form->createView(),
+        ]);
+
+
+
     }
 
 }
