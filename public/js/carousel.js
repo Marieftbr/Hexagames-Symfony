d05f05ebb60d4f7c2217e02725cb4762bc83693b
Marie Gigaud <mariegoulouzelle@gmail.com>
2020-12-09 23:17:07 +0100

    page user

---
+++ public/js/carousel.js
@@ -0,0 +1,36 @@
+$(function () {
+    $(".carousel-icon-right").click(function () {
+        $(".profile").each(function () {
+            // JQuery return the actual px value instead of percentage so back to vanilla js
+            const positionString = $(this)[0].style.left || "0%";
+            const positionValue = parseInt(positionString.slice(0, -1));
+
+            const newValue = positionValue - 25 + "%";
+
+            $(this).css("left", newValue);
+        });
+    });
+
+    $(".carousel-icon-left").click(function () {
+        $(".profile").each(function () {
+            // JQuery return the actual px value instead of percentage so back to vanilla js
+            const positionString = $(this)[0].style.left || "0%";
+            const positionValue = parseInt(positionString.slice(0, -1));
+
+            const newValue = positionValue + 25 + "%";
+
+            $(this).css("left", newValue);
+        });
+    });
+
+    $(".tab").click(function($event) {
+        $event.preventDefault();
+        const target = $(this).attr("href");
+
+        $(".tab").removeClass("tab-active");
+        $(".tab-content").hide();
+
+        $(target).show();
+        $(this).addClass("tab-active");
+    })
+});
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +17,17 @@ class UserController extends AbstractController
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/user/profile/{id}", name="user_profile_id")
+     */
+    public function show(User $user)
+    {
+
+        return $this->render('user/index.html.twig',[
+            'user' => $user
         ]);
     }
 }
